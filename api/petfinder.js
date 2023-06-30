4b3ff8014876f5eb1b06b7198cdfb350ecc1c738
Cameron Hammel <camhammel@gmail.com>
2023-06-30 12:01:52 -0400

    use interceptors

--- api/petfinder.js
+++ api/petfinder.js
@@ -1,16 +1,43 @@
 import axios from "axios";
 import { storage } from "../utils/storage";
 
-export default axios.create({
+
+const petfinderApi = axios.create({
   baseURL: "https://api.petfinder.com/v2/",
 });
 
+petfinderApi.interceptors.request.use(
+  async config => {
+    const token = storage.getString('token')
+    config.headers = { 
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+    }
+    return config;
+  },
+  error => {
+    Promise.reject(error)
+})
+
+petfinderApi.interceptors.response.use((response) => {
+  return response
+}, async function (error) {
+  const originalRequest = error.config;
+  if (error.response.status === 403 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    const access_token = await retrieveToken();            
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
+    return petfinderApi(originalRequest);
+  }
+  return Promise.reject(error);
+})
+
+export default petfinderApi;
+
 export async function retrieveToken() {
-  axios.create()
-    .request({
-      baseURL: "https://api.petfinder.com/v2/",
+  return petfinderApi
+    .post({
       url: "oauth2/token",
-      method: "post",
       data: {
         grant_type: "client_credentials",
         client_id: process.env.PETFINDER_KEY,
@@ -23,6 +50,7 @@ export async function retrieveToken() {
           "token",
           response.data.access_token
         );
+        return response.data.access_token;
       } else {
         console.error('could not retrieve access token from petfinder API', response);
       }
@@ -30,14 +58,9 @@ export async function retrieveToken() {
     .catch((error) => {
       console.error('error', error);
     });
-
-  return;
 }
 
 export async function searchApi({ location, type, age, distance, breed, pageParam = 1}) {
-  await retrieveToken();
-  const token = storage.getString('token');
-  
   const this_search = {
     type,
     limit: 20,
@@ -51,19 +74,11 @@ export async function searchApi({ location, type, age, distance, breed, pagePara
 
   let filtered = Object.fromEntries(Object.entries(this_search).filter(([_, v]) => v != null && v != [] && v != ''));
 
-  return axios
-    .get(`animals?${new URLSearchParams(filtered).toString()}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      baseURL: "https://api.petfinder.com/v2/"
-    })
+  return petfinderApi
+    .get(`animals?${new URLSearchParams(filtered).toString()}`)
 };
 
 export async function getShelterAnimals({ id, pageParam = 1}) {
-  await retrieveToken();
-  const token = storage.getString('token');
-  
   const this_search = {
     limit: 20,
     organization: id,
@@ -72,11 +87,6 @@ export async function getShelterAnimals({ id, pageParam = 1}) {
 
   let filtered = Object.fromEntries(Object.entries(this_search).filter(([_, v]) => v != null && v != [] && v != ''));
 
-  return axios
-    .get(`animals?${new URLSearchParams(filtered).toString()}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      baseURL: "https://api.petfinder.com/v2/"
-    })
+  return petfinderApi
+    .get(`animals?${new URLSearchParams(filtered).toString()}`)
 };
